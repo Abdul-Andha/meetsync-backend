c9000b3a18bf9691d979064125fcc2b3bc90ee2b
abdul-andha <aandha8342@gmail.com>
2025-02-17 16:03:39 +0000

    update docker to just deploy to 443 on the instance

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import uvicorn
 import os
 from fastapi import FastAPI
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 app = FastAPI()
 
 
@@ -11,15 +13,16 @@ async def root():
 
 
 if __name__ == "__main__":
-    ENV = os.getenv("ENV", "dev")
-
-    if ENV == "prod":
+    IS_PROD = config["IS_PROD"] == "True"
+    if IS_PROD:
+        print("Deploying to Prod")
         uvicorn.run(
             app,
             host="0.0.0.0",
             port=443,
-            ssl_keyfile="/etc/letsencrypt/live/api.meet-sync.us/privkey.pem",
-            ssl_certfile="/etc/letsencrypt/live/api.meet-sync.us/fullchain.pem",
+            ssl_keyfile="/certs/privkey.pem",
+            ssl_certfile="/certs/fullchain.pem",
         )
     else:
+        print("Deploying to Dev")
         uvicorn.run(app, host="0.0.0.0", port=8000)
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-ENV = $1
-
-if [ "$ENV" = "prod" ]; then
-    export APP_ENV=dev
-else
-    export APP_ENV=prod
-fi
-
 git pull origin main
 
 sudo docker-compose down
