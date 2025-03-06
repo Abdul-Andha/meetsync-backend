9d515bbf671f2c4cfe0b148ddf9aa4af3661d59d
Abdul-Andha <aandha8342@gmail.com>
2025-03-06 13:26:44 -0500

    add participants to proper table

--- app/custom_errors.py
+++ app/custom_errors.py
@@ -2,5 +2,9 @@ class InvalidUser(Exception):
     """User ID can not be null"""
 
 
+class InvalidHangout(Exception):
+    """Hangout ID can not be null"""
+
+
 class UnexpectedError(Exception):
     """Use this class for all unexpected exeptions"""
