17a4f6b1c49839db357a13270199292e60a184ac
IshmamF <ishmamf2003@gmail.com>
2025-04-01 15:53:47 -0400

    added/updated statuses in types for notification and hangouts

--- app/custom_types.py
+++ app/custom_types.py
@@ -5,6 +5,9 @@ class HangoutStatus(str, Enum):
     INVITES_SENT = "invites_sent"
     FETCHING_AVAILABILITY = "fetching-availability"
     DETERMINING_LOCATION = "determining-location"
+    CONFIRM_TIME = 'confirm-time'
+    CONFIRM_MEETUP = 'confirm-meetup'
+    CONFIRM = 'confirmed'
 
 
 class InviteeStatus(str, Enum):
@@ -16,3 +19,12 @@ class InviteeStatus(str, Enum):
 class FriendStatus(str, Enum):
     PENDING = "pending"
     ACCEPTED = "accepted"
+
+class NotificationType(str, Enum):
+    HANGOUT_INVITE = 'hangout-invite'
+    FRIEND_REQUEST = 'friend-request'
+    GENERAL = 'general'
+    SELECT_AVAILABILITY = 'select-availability'
+    SELECT_PLACRES = 'select-places'
+    CONFIRM_MEETUP = 'confirm-meetup'
+    CONFIRM_TIME = 'confirm-time'
